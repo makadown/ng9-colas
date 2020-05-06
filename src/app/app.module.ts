c8caab9468d107ee30f63d496c3381f64d9d0df9
makadown <maseflores79@gmail.com>
2020-05-05 22:07:22 -0500

    aplicacion preparada para iniciar proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EscritorioComponent } from './pages/escritorio/escritorio.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NuevoTicketComponent } from './pages/nuevo-ticket/nuevo-ticket.component';
+import { PublicoComponent } from './pages/publico/publico.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EscritorioComponent,
+    HomeComponent,
+    NuevoTicketComponent,
+    PublicoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/publico/publico.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publico',
+  templateUrl: './publico.component.html',
+  styleUrls: ['./publico.component.css']
+})
+export class PublicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+   /*  const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('container'); */
+  }
+
+}
